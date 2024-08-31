bd439b273a1218044d3baf238334f3216578cbe4
cocktailpeanut <cocktailpeanuts@proton.me>
2024-08-31 19:29:42 -0400

    update

--- install.js
+++ install.js
@@ -5,39 +5,68 @@ module.exports = {
       method: "shell.run",
       params: {
         message: [
-          "git clone https://github.com/bmaltais/kohya_ss app",
+          //"git clone --recursive https://github.com/bmaltais/kohya_ss -b sd3-flux.1 --single-branch app",
+          "git clone --recursive https://github.com/peanutcocktail/kohya_ss -b sd3-flux.1 --single-branch app",
         ]
       }
     },
-    // Delete this step if your project does not use torch
     {
-      method: "script.start",
+      method: "fs.copy",
       params: {
-        uri: "torch.js",
-        params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          // xformers: true   // uncomment this line if your project requires xformers
+        src: "dataset/dataset.toml",
+        dest: "app/sd-scripts/dataset.toml"
+      }
+    },
+    {
+      method: "fs.copy",
+      params: {
+        src: "dataset/{{platform === 'win32' ? 'train.bat' : 'train.sh'}}",
+        dest: "app/sd-scripts/{{platform === 'win32' ? 'train.bat' : 'train.sh'}}"
+      }
+    },
+    {
+      method: "fs.copy",
+      params: {
+        src: "dataset/images",
+        dest: "app/dataset/images",
+        options: {
+          force: true
         }
       }
     },
+//    // Delete this step if your project does not use torch
+//    {
+//      method: "script.start",
+//      params: {
+//        uri: "torch.js",
+//        params: {
+//          venv: "env",                // Edit this to customize the venv folder path
+//          path: "app",                // Edit this to customize the path to start the shell from
+//          // xformers: true   // uncomment this line if your project requires xformers
+//        }
+//      }
+//    },
     // Edit this step with your custom install commands
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
         path: "app",                // Edit this to customize the path to start the shell from
         message: [
-          "pip install gradio devicetorch",
-          "pip install -r requirements.txt"
+          "{{platform === 'win32' ? 'setup.bat' : 'bash setup.sh'}}",
         ]
       }
     },
     {
-      method: "fs.link",
+      method: "fs.download",
       params: {
-        venv: "app/env"
+        uri: [
+          "https://huggingface.co/cocktailpeanut/xulf-dev/resolve/main/flux1-dev.sft?download=true",
+          "https://huggingface.co/cocktailpeanut/xulf-dev/resolve/main/ae.sft?download=true",
+          "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/clip_l.safetensors?download=true",
+          "https://huggingface.co/comfyanonymous/flux_text_encoders/resolve/main/t5xxl_fp16.safetensors?download=true",
+        ],
+        dir: "app/models"
       }
-    }
+    },
   ]
 }
